d80eb4cddb4b715a5ba8ba6a0d9d75a4914861ad
Derek Fan <derekfan.18.2002@gmail.com>
2025-04-07 15:30:04 -0400

    added exports

--- src/HybridRobotDynamics.jl
+++ src/HybridRobotDynamics.jl
@@ -2,6 +2,15 @@ module HybridRobotDynamics
 
 using RigidBodyDynamics
 
+export
+        add_transition!,
+        Transition,
+        HybridMode,
+        HybridSystem,
+        bouncing_ball,
+        hopper
+
 include("utils.jl")
+include("models.jl")
 
 end # module HybridRobotDynamics
