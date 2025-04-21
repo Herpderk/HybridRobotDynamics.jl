3165818a1eda15c2efa1391f6eefcc79a0b5eecb
Joshua Ramos <joshuara@andrew.cmu.edu>
2025-04-21 09:27:19 -0400

    kinda working rabbit

--- examples/five_link_walker.jl
+++ examples/five_link_walker.jl
@@ -2,6 +2,7 @@ using Pkg; Pkg.activate(joinpath(@__DIR__, ".."))
 using LinearAlgebra
 using HybridRobotDynamics
 
+<<<<<<< Updated upstream
 # Define RABBIT (Five-Link Walker) model parameters
 g = 9.81  # gravity
 
@@ -13,11 +14,206 @@ ms = [5.0, 5.0, 5.0, 5.0, 5.0]
 
 # Link inertias (kg*m^2) - planar scalar Izz only
 Is = [0.1, 0.1, 0.1, 0.1, 0.1] 
+=======
+using Plots
+gr()
+rm("walker.gif")
+
+function compute_joint_positions(q::AbstractVector, ls::AbstractVector)
+  x_com, y_com = q[1:2]
+  θ₁, θ₂, θ₃, θ₄, θ₅ = q[3:7]
+  l1, l2, l3, l4, l5 = ls
+
+  hip = [x_com, y_com]
+
+  θ_torso      = θ₅
+  θ_stance_th  = θ_torso + θ₂
+  θ_stance_leg = θ_stance_th + θ₁
+  θ_swing_th   = θ_torso + θ₄
+  θ_swing_leg  = θ_swing_th + θ₃
+
+  torso_tip     = hip .+ l5 * [cos(θ_torso), sin(θ_torso)]
+
+  stance_thigh  = hip .- l2 * [cos(θ_stance_th), sin(θ_stance_th)]
+  stance_foot   = stance_thigh .- l1 * [cos(θ_stance_leg), sin(θ_stance_leg)]
+
+  swing_thigh   = hip .- l4 * [cos(θ_swing_th), sin(θ_swing_th)]
+  swing_foot    = swing_thigh .- l3 * [cos(θ_swing_leg), sin(θ_swing_leg)]
+
+  return [
+    hip stance_thigh stance_foot swing_thigh swing_foot torso_tip
+    ]
+end
+
+function animate_walker(xs, ls, N, nx; title="Five-Link Walker", fps=20, gifname="walker.gif")
+  anim = @animate for k in 1:N
+      xk = xs[(k-1)*nx+1 : k*nx]
+      joints = compute_joint_positions(xk, ls)
+
+      # Extract x and y for each joint
+      xpts = joints[1, :]
+      ypts = joints[2, :]
+
+      # Base scatter plot of all joints
+      p = scatter(
+          xpts, ypts,
+          label = false,
+          markersize = 5,
+          markercolor = :blue,
+          xlim = (-2, 3),
+          ylim = (-2, 3),
+          title = title,
+          xlabel = "x",
+          ylabel = "y",
+          aspect_ratio = :equal,
+      )
+
+      # Add physical links (manually between joint indices)
+      # Format: (joint1, joint2)
+      links = [
+          (1, 2),  # hip to stance thigh
+          (2, 3),  # stance thigh to stance foot
+          (1, 4),  # hip to swing thigh
+          (4, 5),  # swing thigh to swing foot
+          (1, 6)   # hip to torso tip
+      ]
+
+      for (i, j) in links
+
+          if ((i,j) == (1, 2)) 
+            _color = :green; 
+          elseif ((i,j) == (2, 3)) 
+            _color = :blue; 
+          elseif ((i,j) == (1, 4))
+             _color = :yellow; 
+          elseif ((i,j) == (4, 5)) 
+            _color = :red; 
+          elseif ((i,j) == (1, 6)) 
+            _color = :black; 
+          else
+            _color = :white;
+          end
+          
+          plot!([xpts[i], xpts[j]], [ypts[i], ypts[j]], lw=2, color=_color, label=false)
+      end
+  end
+
+  gif(anim, gifname, fps = fps)
+end
+
+function draw_walker(x, ls) 
+  # Unpack state
+  x_com, y_com = x[1:2]
+  θ₁, θ₂, θ₃, θ₄, θ₅ = x[3:7]
+  l1, l2, l3, l4, l5 = ls
+
+  # Hip is the floating base
+  hip = [x_com; y_com]
+
+  # Absolute angles for each link
+  θ_torso       = θ₅
+  θ_stance_th   = θ_torso + θ₂
+  θ_stance_leg  = θ_stance_th + θ₁
+  θ_swing_th    = θ_torso + θ₄
+  θ_swing_leg   = θ_swing_th + θ₃
+
+  # Compute joint positions
+  torso_tip     = hip .+ l5 * [cos(θ_torso), sin(θ_torso)]
+  stance_thigh  = hip .- l2 * [cos(θ_stance_th), sin(θ_stance_th)]
+  stance_foot   = stance_thigh .- l1 * [cos(θ_stance_leg), sin(θ_stance_leg)]
+  swing_thigh   = hip .- l4 * [cos(θ_swing_th), sin(θ_swing_th)]
+  swing_foot    = swing_thigh .- l3 * [cos(θ_swing_leg), sin(θ_swing_leg)]
+
+  joints =  Dict(
+    "torso_tip"     => torso_tip,
+    "hip"           => hip,
+    "stance_thigh"  => stance_thigh,
+    "stance_foot"   => stance_foot,
+    "swing_thigh"   => swing_thigh,
+    "swing_foot"    => swing_foot
+  )
+
+  # Plot setup
+  plt = plot(; aspect_ratio=1, legend=false, title="walker model", xlabel="x", ylabel="y")
+
+  # Plot links
+  plot!([torso_tip[1], hip[1]], [torso_tip[2], hip[2]], lw=3, color=:black, label="torso")
+  plot!([hip[1], stance_thigh[1]], [hip[2], stance_thigh[2]], lw=3, color=:green, label="stance thigh")
+  plot!([stance_thigh[1], stance_foot[1]], [stance_thigh[2], stance_foot[2]], lw=3, color=:blue, label="stance leg")
+  plot!([hip[1], swing_thigh[1]], [hip[2], swing_thigh[2]], lw=3, color=:orange, label="swing thigh")
+  plot!([swing_thigh[1], swing_foot[1]], [swing_thigh[2], swing_foot[2]], lw=3, color=:red, label="swing leg")
+
+  # Plot joints
+  for (label, pos) in joints
+      scatter!([pos[1]], [pos[2]]; color=:gray, markersize=6)
+      annotate!(pos[1], pos[2] + 0.05, text(label, :black, 8))
+  end
+
+  # CoM marker
+  scatter!([x_com], [y_com]; marker=:x, color=:magenta, label="CoM")
+
+  display(plt)
+  savefig("walker.png")
+end
+
+
+# Define RABBIT (Five-Link Walker) model parameters
+g = -9.81  # gravity
+
+# Link lengths (meters)
+ls = [
+    0.4,  # l1: stance leg
+    0.5,  # l2: stance thigh
+    0.4,  # l3: swing leg
+    0.5,  # l4: swing thigh
+    0.6   # l5: torso
+]
+
+# Link masses (kg)
+ms = [
+    5.0,   # m1: stance leg
+    7.0,   # m2: stance thigh
+    5.0,   # m3: swing leg
+    7.0,   # m4: swing thigh
+    10.0   # m5: torso
+]
+
+# Link inertias (kg·m²)
+Is = [
+    (1/12) * ms[1] * ls[1]^2,  # I1: stance leg
+    (1/12) * ms[2] * ls[2]^2,  # I2: stance thigh
+    (1/12) * ms[3] * ls[3]^2,  # I3: swing leg
+    (1/12) * ms[4] * ls[4]^2,  # I4: swing thigh
+    (1/12) * ms[5] * ls[5]^2   # I5: torso
+]
+
+# # Initial conditions (position + velocity)
+# # q1...q7, dq1...dq7
+xic = [
+    0.0, 0.75,                  # x_com, y_com
+    π/5, -π/4, -π/5, π/4, π/2,  # θ₁ to θ₅
+    0.0, 0.0,                   # ẋ_com, ẏ_com
+    0.0, 0.0, 0.0, 0.0, 0.0     # joint velocities
+]
+
+function generate_stand_us(N::Int, dt::Float64; amp1=10.0, amp2=10.0, freq=1.0)
+  # Create sinusoidal torque profiles for two hip actuators
+  us = zeros(N * 2)
+  for k in 1:2:2*N-1
+      us[k] =  34 # Hip 1 (stance) // +:CCW    -:CW
+      us[k+1] =  -23 # Hip 2 (swing) //  +:CW     -:CCW
+  end
+  return us
+end
+
+draw_walker(xic, ls)
+>>>>>>> Stashed changes
 
 # Generate the hybrid system
 system = five_link_walker(Is, ls, ms, g)
 
 # Roll-out simulation parameters
+<<<<<<< Updated upstream
 N = 100               # number of timesteps
 Δt = 0.05             # timestep duration (s)
 
@@ -30,6 +226,16 @@ xic = [0.0, -0.2, 0.2, -0.1, 0.0,
        0.0, 0.0, 0.0, 0.0, 0.0]  
 
 init_mode = :stance
+=======
+N =  100               # number of timesteps
+Δt = 0.05             # timestep duration (s)
+
+# Control inputs
+# us = zeros(N * system.nu)
+us = generate_stand_us(N-1, Δt)
+
+init_mode = :flight
+>>>>>>> Stashed changes
 
 # Integrator choice
 rk4 = ExplicitIntegrator(:rk4)
@@ -38,5 +244,10 @@ rk4 = ExplicitIntegrator(:rk4)
 xs = roll_out(system, rk4, N, Δt, us, xic, init_mode)
 
 # Plotting configuration
+<<<<<<< Updated upstream
 plot_2d_states(N, system.nx, (1,3), xs; title="Five-Link Walker Roll-Out")
 nothing
+=======
+animate_walker(xs, ls, N, system.nx; title="Walker Rollout", fps=20)
+nothing
+>>>>>>> Stashed changes
--- src/models.jl
+++ src/models.jl
@@ -197,6 +197,7 @@ function hopper(
 end
 
 """
+<<<<<<< Updated upstream
 ## Inputs:
 Is = [I1 I2 I3 I4 I5] :: link inertias (rotational :: Izz)
 ms = [m1 m2 m3 m4 m5] :: link masses
@@ -387,3 +388,306 @@ function five_link_walker(
 
   return HybridSystem(nx, nu, transitions, modes)
 end
+=======
+"""
+function five_link_walker(
+  Is::Vector{<:Real},        # Link inertias
+  ls::Vector{<:Real},        # Link lengths
+  ms::Vector{<:Real},        # Link masses
+  g::Real = 9.81             # Gravitational constant
+)::HybridSystem
+
+    nx = 14  # 5 positions + 5 velocities + 2 COM + 2 COM velocities
+    nu = 2   # 2 actuators (hips)
+
+    # Unpack link parameters
+    l1, l2, l3, l4, l5 = ls
+
+    # Actuation mapping (only joints actuated)
+    B = [   0.0  0.0;  # x_com
+            0.0  0.0;  # y_com
+            0.0  0.0;  # θ₁ = left leg
+            1.0  0.0;  # θ₂ = left thigh (u₁)
+            0.0  0.0;  # θ₃ = right leg
+            0.0  1.0;  # θ₄ = right thigh (u₂)
+            0.0  0.0;  # θ₅ = torso
+    ]
+
+    function com_position(q::AbstractVector, i::Int)
+        hip = q[1:2]
+        θ₁, θ₂, θ₃, θ₄, θ₅ = q[3:7]    
+    
+        if i == 1  # left leg
+            θ_th = θ₅ + θ₂
+            θ_leg = θ_th + θ₁
+            left_thigh = hip .- (l2/2) * [cos(θ_th), sin(θ_th)]
+            return left_thigh .- (l1/2) * [cos(θ_leg), sin(θ_leg)]
+    
+        elseif i == 2  # left thigh
+            θ_th = θ₅ + θ₂
+            return hip .- (l2/2) * [cos(θ_th), sin(θ_th)]
+    
+        elseif i == 3  # right leg
+            θ_th = θ₅ + θ₄
+            θ_leg = θ_th + θ₃
+            right_thigh = hip .- (l4/2) * [cos(θ_th), sin(θ_th)]
+            return right_thigh .- (l3/2) * [cos(θ_leg), sin(θ_leg)]
+    
+        elseif i == 4  # right thigh
+            θ_th = θ₅ + θ₄
+            return hip .- (l4/2) * [cos(θ_th), sin(θ_th)]
+    
+        elseif i == 5  # torso
+            return hip .+ (l5/2) * [cos(θ₅), sin(θ₅)]
+    
+        else
+            error("Invalid link index")
+        end
+    end
+
+     # left foot position and Jacobian
+    function p_left(q::AbstractVector)
+        x_com, y_com = q[1:2]
+        θ₁, θ₂, _, _, θ₅ = q[3:7]
+        l1, l2 = ls[1:2]
+    
+        θ_thigh = θ₅ + θ₂
+        θ_leg   = θ_thigh + θ₁
+    
+        hip = [x_com, y_com]
+        thigh = hip .- l2 * [cos(θ_thigh), sin(θ_thigh)]
+        foot  = thigh .- l1 * [cos(θ_leg), sin(θ_leg)]
+        return foot
+    end
+
+    function left_foot_height(x::AbstractVector)
+        q = x[1:7]
+        return p_left(q)[2]
+    end
+
+    # right foot position and Jacobian
+    function p_right(q::AbstractVector)
+        x_com, y_com = q[1:2]
+        _, _, θ₃, θ₄, θ₅ = q[3:7]
+        l3, l4 = ls[3:4]
+    
+        θ_thigh = θ₅ + θ₄
+        θ_leg   = θ_thigh + θ₃
+    
+        hip = [x_com, y_com]
+        thigh = hip .- l4 * [cos(θ_thigh), sin(θ_thigh)]
+        foot  = thigh .- l3 * [cos(θ_leg), sin(θ_leg)]
+        return foot
+    end
+
+    function right_foot_height(x::AbstractVector)
+        q = x[1:7]
+        return p_right(q)[2]
+    end
+    
+    function total_com(q::AbstractVector)
+        M_total = sum(ms)
+        com = zeros(2)
+        for i in 1:5
+            com += ms[i] * com_position(q, i)
+        end
+        return com / M_total
+    end
+
+    function com_position_from_theta(θ::AbstractVector{<:Real}, i::Int)
+        q = vcat(zeros(eltype(θ), 2), θ)
+        return com_position(q, i)
+    end
+
+    function M(q::AbstractVector{<:Real})
+        θ = q[3:7]
+        θ̇₀ = zeros(eltype(q), 5)
+    
+        T_of_θ̇ = θ̇ -> begin
+            T = 0.0
+            for i in 1:5
+                J_i = ForwardDiff.jacobian(θ -> com_position_from_theta(θ, i), θ)
+                v_i = J_i * θ̇
+                T += 0.5 * ms[i] * dot(v_i, v_i) + 0.5 * Is[i] * θ̇[i]^2
+            end
+            return T
+        end
+    
+        M_joint = ForwardDiff.hessian(T_of_θ̇, θ̇₀)
+    
+        m_total = sum(ms)
+        M_full = zeros(eltype(q), 7, 7)
+        M_full[1:2, 1:2] .= m_total * I(2)     # ẋ_com and ẏ_com
+        M_full[3:7, 3:7] .= M_joint            # joint-space
+        return M_full
+    end
+
+    function C(q::AbstractVector{<:Real}, q̇::AbstractVector{<:Real})
+        θ = q[3:7]
+        θ̇ = q̇[3:7]
+        n = length(θ)
+    
+        Cθ = zeros(eltype(q), n, n)
+    
+        # Mθ: M as a function of θ only
+        function Mθ(θ₁::AbstractVector)
+            q_full = vcat([0.0, 0.0], θ₁)
+            return M(q_full)[3:7, 3:7]     # extract joint part
+        end
+    
+        # Loop over Christoffel symbols to construct joint-space C matrix
+        for i in 1:n, j in 1:n
+            for k in 1:n
+                ∂M_ij_∂θk = ForwardDiff.gradient(θ₁ -> Mθ(θ₁)[i, j], θ)[k]
+                ∂M_ik_∂θj = ForwardDiff.gradient(θ₁ -> Mθ(θ₁)[i, k], θ)[j]
+                ∂M_jk_∂θi = ForwardDiff.gradient(θ₁ -> Mθ(θ₁)[j, k], θ)[i]
+    
+                Cθ[i, j] += 0.5 * (∂M_ij_∂θk + ∂M_ik_∂θj - ∂M_jk_∂θi) * θ̇[k]
+            end
+        end
+    
+        # Embed into full 7×7 matrix
+        C_full = zeros(eltype(q), 7, 7)
+        C_full[3:7, 3:7] .= Cθ
+    
+        return C_full
+    end
+    
+    function G(q::AbstractVector{<:Real})
+        θ = q[3:7]
+        Gθ = zeros(eltype(q), 5)
+    
+        for i in 1:5
+            y_i = θ -> com_position(vcat(zeros(2), θ), i)[2]
+            Gθ[i] = -ms[i] * g * ForwardDiff.gradient(y_i, θ)[i]
+        end
+    
+        G_full = zeros(eltype(q), 7)
+        G_full[3:7] .= Gθ
+        G_full[2] = -sum(ms) * g   # gravity acts downward on total mass
+        return G_full
+    end
+
+    function c(q, q̇)
+      return C(q, q̇) * q̇ + G(q)
+    end
+  
+    function reset_velocity_after_impact(x, p_foot_fn)
+        q = x[1:7]
+        q̇ = x[8:14]
+    
+        Mq = M(q)                        # 7x7
+
+        Jc = ForwardDiff.jacobian(p_foot_fn, q)  # 2×7
+    
+        A = [Mq -Jc'; Jc zeros(2, 2)]
+        rhs = [Mq * q̇; zeros(2)]
+    
+        sol = A \ rhs
+        q̇_post = sol[1:7]
+    
+        return vcat(q, q̇_post)
+    end
+
+    function foot_jacobian_and_jacdot(p_foot, q, q̇)
+      J = ForwardDiff.jacobian(p_foot, q)
+      J̇ = ForwardDiff.jacobian(q_ -> ForwardDiff.jacobian(p_foot, q_) * q̇, q)
+      return J, J̇
+  end
+
+    function walker_flow(
+        M_f::Function,
+        C_f::Function,
+        G_f::Function,
+        B::Matrix{Float64},  # 5×2 actuation mapping
+        x::Vector{<:DiffFloat},  # full state: [q; q̇], 14×1
+        u::Vector{<:DiffFloat},  # input torque: 2×1
+        mode::Symbol             # symbol to discern current mode
+    )::Vector{<:DiffFloat}
+    
+        # Split state
+        q = x[1:7]
+        q̇ = x[8:14]
+
+        # Evaluate dynamics
+        Mq = M_f(q)       # 7×7
+        Cq = C_f(q, q̇)    # 7×7
+        Gq = G_f(q)       # 7×1
+        Bu = B * u        # 7×1
+
+        if mode == :flight
+            q̈ = Mq \ (Bu - Cq * q̇ - Gq)
+            return vcat(q̇, q̈)
+        end
+        
+        # Select contact Jacobian
+        Jc, J̇c = (mode == :left)  ? foot_jacobian_and_jacdot(p_left, q, q̇) :
+                (mode == :right) ? foot_jacobian_and_jacdot(p_right, q, q̇) :
+                error("Unknown mode: $mode")
+
+        # KKT system
+        A = [Mq  -Jc'; 
+            Jc   zeros(eltype(q), size(Jc, 1), size(Jc, 1))]
+
+        rhs = [Bu - Cq * q̇ - Gq;
+            -J̇c * q̇]
+
+        sol = A \ rhs
+        q̈ = sol[1:7]
+
+        return vcat(q̇, q̈)
+    end
+
+    left_flow = (
+        x::Vector{<:DiffFloat},
+        u::Vector{<:DiffFloat}
+    ) -> walker_flow(M, C, G, B, x, u, :left)
+
+    right_flow = (
+        x::Vector{<:DiffFloat},
+        u::Vector{<:DiffFloat}
+    ) -> walker_flow(M, C, G, B, x, u, :right)
+
+    flight_flow = (
+        x::Vector{<:DiffFloat},
+        u::Vector{<:DiffFloat}
+    ) -> walker_flow(M, C, G, B, x, u, :flight)
+
+    modes = Dict(
+        :flight  => HybridMode(flight_flow),
+        :left    => HybridMode(left_flow),
+        :right   => HybridMode(right_flow),
+    )
+
+    g_FL = x -> left_foot_height(x) 
+    R_FL = x -> reset_velocity_after_impact(x, p_left)
+    impact_FL = Transition(flight_flow, left_flow, g_FL, R_FL)
+
+    g_RL = x -> left_foot_height(x) 
+    R_RL = x -> reset_velocity_after_impact(x, p_left)
+    impact_RL = Transition(right_flow, left_flow, g_RL, R_RL)
+    
+    g_FR = x -> right_foot_height(x) 
+    R_FR = x -> reset_velocity_after_impact(x, p_right)
+    impact_FR = Transition(flight_flow, right_flow, g_FR, R_FR)
+
+    g_LR = x -> right_foot_height(x) 
+    R_LR = x -> reset_velocity_after_impact(x, p_right)
+    impact_LR = Transition(left_flow, right_flow, g_LR, R_LR)
+
+    add_transition!(modes[:flight], modes[:left], impact_FL)
+    add_transition!(modes[:flight], modes[:right], impact_FR)
+    
+    add_transition!(modes[:left], modes[:right], impact_LR)
+    add_transition!(modes[:right], modes[:left], impact_RL)
+    
+    transitions = Dict(
+        :impact_FL => impact_FL,
+        :impact_FR => impact_FR,
+        :impact_RL => impact_RL,
+        :impact_LR => impact_LR,
+    )
+
+    return HybridSystem(nx, nu, transitions, modes)
+end
+>>>>>>> Stashed changes
